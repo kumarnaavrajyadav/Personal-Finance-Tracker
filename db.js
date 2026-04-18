2d7b4c6ffaa92e08934f4d308fe3634862455723
Navraj Kumar Yadav <navrajyadavkuumar@gmail.com>
2026-04-18 22:33:04 +0530

    Updated backend API and fixed bugs

--- db.js
+++ db.js
@@ -1,17 +1,19 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-  host: "127.0.0.1", // Changed from localhost to 127.0.0.1 for stability on Mac
-  user: "root",
-  password: "Navraj@#123",
-  database: "finance_tracker"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 db.connect(err => {
   if (err) {
-    console.error("DB Connection Failed:", err);
+    console.error("Railway DB Connection Failed:", err);
   } else {
-    console.log("✅ Connected to MySQL Database");
+    console.log("Connected to Railway MySQL ✅");
   }
 });
 
